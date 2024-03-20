087255608d5c8397025e69a81e97c165f2f217cb
AadelAferyad <animevineu@gmail.com>
2024-03-20 17:06:08 +0000

    update and add new methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            filtred_dict = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    filtred_dict[key] = value
+            return filtred_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +51,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        delete obj from __objects if its inside - if obj is equal to None,
+        the method should not do anything
+        """
+        if obj:
+            self.all().pop(obj.to_dict()['__class__'] + '.' + obj.id)
