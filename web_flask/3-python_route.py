4622301c6e478e000494b5fd8714fab31ff3f3db
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-04-21 21:23:13 +0100

    8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""FLASK FRAMEWORK"""
-from flask import Flask
+"""3-Starts a Flask web application"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def start():
-    """Return Hello world"""
-    return f"Hello HBNB!"
+def hello():
+    """say hello"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return hbnb"""
-    return f"HBNB"
+    """show HBNB"""
+    return 'HBNB'
 
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """show the script"""
-    return f'C {text.replace("_", " ")}'
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def py(text):
-    """show the script"""
+def python(text='is_cool'):
+    """show variable text"""
     return f'Python {text.replace("_", " ")}'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
