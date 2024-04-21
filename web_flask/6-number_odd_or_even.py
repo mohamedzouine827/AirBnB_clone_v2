fa0acbc9454f695501c81de10fae8b1e269d09ca
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-04-21 22:14:15 +0100

    8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,13 +44,7 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """show the script"""
-    className = ""
-    if n % 2 == 0:
-        className = "odd"
-    else:
-        className = "even"
-    return render_template("6-number_odd_or_even.html", n=n,
-                           className=className)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
