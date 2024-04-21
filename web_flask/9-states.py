f2dedc076c3e6afe16ec1a6dc3793c1804cb9bbe
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-04-21 23:06:59 +0100

    8

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""9-Start a Flask web application."""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """states returned"""
+    return render_template('9-states.html\
+', states=storage.all("State"), state=None)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def id_state(id):
+    """states returned"""
+    states = storage.all("State")
+    # if "State.{}".format(id) not in states:
+    #     return render_template('9-states.html')
+    state = storage.all("State").get("State.{}".format(id))
+    return render_template('9-states.html\
+', state=state, states=None)
+
+
+@app.teardown_appcontext
+def reset(error):
+    """reload data"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
